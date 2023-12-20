a436ec41db42c458d522ace8eca3e220ffb78d6b
akrom17ua <akromurnashev17@gmail.com>
2023-12-20 22:58:15 +0500

    ch

--- backend/ecom/serializers.py
+++ backend/ecom/serializers.py
@@ -1,12 +1,42 @@
 from rest_framework import serializers
 from .models import Product
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
 
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'token']
+
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
